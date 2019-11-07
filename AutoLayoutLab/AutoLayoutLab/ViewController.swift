6ad467e71d344acaeaccd8dd12cd70d23958be37
tanyaburke <tanyaburke@pursuit.org>
2019-11-07 15:28:11 -0500

    Initial Attempt

---
+++ AutoLayoutLab/AutoLayoutLab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayoutLab
+//
+//  Created by Tanya Burke on 11/7/19.
+//  Copyright © 2019 Tanya Burke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
